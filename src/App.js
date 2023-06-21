6d87541fafcd056f66a29a04cd12a069ae3ee4e3
Kyllian Warella <2159135@talnet.nl>
2023-06-21 16:34:20 +0200

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      myString: ''
+    }
+    this.getNumbers();
+  }
+  
+  getNumbers() {
+    for (var i=1; i < 11; i++) {
+      this.state.myString = this.state.myString + i + ", ";
+      
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        {this.state.myString}
+        <br/>
+        Kyllian
+      </div>
+    );
+  }
+
 }
 
 export default App;
